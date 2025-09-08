3a00268819dd9f28f963437b1952ed7f329b224d
xxsummi <jeskhasamanthaderama@gmail.com>
2025-09-08 12:11:20 +0800

    added inventory backend

---
+++ InventorySystem/backend/InventorySystem/InventorySystem/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventorySystem.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Sku { get; set; } = string.Empty;
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Stock { get; set; } = 0;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
